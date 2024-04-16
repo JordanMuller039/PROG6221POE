7dd140af568bd974d15be5e0ae606d6ac769359e
Jordan Muller <st10150702@vcconnect.edu.za>
2024-04-16 19:42:40 +0200

    Added POE Part 1
    Added the final code for PROG6221 POE Part 1

---
+++ POE/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE
+{
+    internal class Program
+    {
+        //Creating Multiply and Divide Variables which will later be given a value by the user
+        static int Divide = 1, Multiply = 1;
+
+        //Main Method which just gives the UserChoice method so that the program knows what the user wants to do
+        static void Main(string[] args)
+        {
+            String MenuChoice = UserChoice();
+            RecipeOperations(MenuChoice);   
+        }
+
+
+        //This method gives the user a choice for them so that they can create or view methods
+        public static String UserChoice()
+        {
+            Console.WriteLine("\tMenu");
+            Console.WriteLine("1) New Recipe");
+            Console.WriteLine("2) View Recipe");
+            Console.WriteLine("3) Scale Recipe");
+            Console.WriteLine("4) Reset Scale");
+            Console.WriteLine("5) Clear Recipe");
+            String choice = Console.ReadLine();
+            return choice;
+        }
+
+        //Once the user has chosen an option it will then run this method which takes the input and decides what opertation to run
+        public static void RecipeOperations(String MenuChoice) 
+        {
+            RecipeOperations recipeOPs = new RecipeOperations();
+            String mc = "";
+            //If user chose to Create a recipe
+            if (MenuChoice == "1")
+            {
+                recipeOPs.CreateRecipe();
+                Console.WriteLine("\n");
+                mc = UserChoice();
+                RecipeOperations(mc);
+            }
+            //If user chose to view the recipe they just made
+            else if (MenuChoice == "2")
+            {
+                Console.WriteLine(recipeOPs.ToString());
+                Console.WriteLine("\n");
+                mc = UserChoice();
+                RecipeOperations(mc);
+            }
+            //If the User chose to ratio the quantity
+            else if (MenuChoice == "3")
+            {
+                Console.WriteLine("Type D for Divide or M for Multiply");
+                String Ratio = Console.ReadLine().ToLower();
+                if(Ratio == "d") 
+                {
+                    Console.WriteLine("Enter Amount to Divide by:");
+                    Divide = Convert.ToInt32(Console.ReadLine());
+                    recipeOPs.DivideQuantity(Divide);
+                    Console.WriteLine(recipeOPs.ToString());
+                    mc = UserChoice();
+                    RecipeOperations(mc);
+
+                }
+                else if(Ratio == "m")
+                {
+                    Console.WriteLine("Enter Amount to Multiply by:");
+                    Multiply = Convert.ToInt32(Console.ReadLine());
+                    recipeOPs.MultiplyQuantity(Multiply);
+                    Console.WriteLine(recipeOPs.ToString());
+                    mc = UserChoice();
+                    RecipeOperations(mc);
+                }
+            }
+            //if the User chose to reset the ratio of the quantity back to its original 
+            else if (MenuChoice == "4")
+            {
+                recipeOPs.ResetQuant();
+                Console.WriteLine("\n");
+                mc = UserChoice();
+                RecipeOperations(mc);
+            }
+            //If the user chose to delete/clear the recipe from memory
+            else if (MenuChoice == "5")
+            {
+                
+            }
+        }
+    }
+}
---
+++ POE/RecipeOperations.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+//References
+// 1    https://www.loginradius.com/blog/engineering/how-to-create-and-use-dictionary-csharp/
+// 2    https://stackoverflow.com/questions/1070766/editing-dictionary-values-in-a-foreach-loop
+
+
+namespace POE
+{
+    internal class RecipeOperations
+    {
+        //Creating the Dictionaries 
+        static String[] arrSteps;
+        static Dictionary<string, double> dictIngredient = new Dictionary<string, double>();
+        static Dictionary<string, string> dictType = new Dictionary<string, string>();
+
+        //Creating variables for if the user chages the ratio so that I can reset the ratio later
+        static Boolean isMultiplied = false, isDivide = false;
+        static int M = 0, D = 0;
+
+        //Defualt Constructor
+        public RecipeOperations() 
+        {
+            
+        }
+
+
+        //Create Recipe Method which will take in the users input and then create a recipe from that s
+        public void CreateRecipe()
+        {
+            Console.WriteLine("How many ingredients in this recipe?");
+            int NumIngredients = Convert.ToInt32(Console.ReadLine());
+
+            //Im using 2 dictionaries that share the same Key however have different values thereby linking them, similar to a parallel array 
+            //The shared Key being the Ingredient Name as it is a unique identifier
+            for (int i = 0; i < NumIngredients; i++)
+            {
+                Console.WriteLine("Please enter the Name of the " + (i+1) + " ingredient");
+                String IngredientName = Console.ReadLine();
+
+                Console.WriteLine("Please enter the Quantity of the Ingredient " + (i+1));
+                double IngredientQuantity = Convert.ToDouble(Console.ReadLine());
+
+                //Adding to dictIngredient
+                dictIngredient.Add(IngredientName, IngredientQuantity);
+
+                Console.WriteLine("Please enter the Measurement of the Ingredient " + (i+1));
+                String MeasurementType = Console.ReadLine();
+
+                //Adding to the dictType
+                dictType.Add(IngredientName, MeasurementType);
+
+            }
+
+            Console.WriteLine("Recipe Created");
+
+        }
+
+        //Overriden ToString method to display to the user the recipe they have created
+        public override string ToString()
+        {
+            String Output = "***************************\nIngredients:";
+            for (int i = 0; i < dictIngredient.Count; i++)
+            {
+                Output = Output + "\n" + dictIngredient.ElementAt(i).Value + " " + dictType.ElementAt(i).Value + " " + dictIngredient.ElementAt(i).Key;
+            }
+            Output = Output + "\n***************************";
+
+            return Output;
+        }
+
+        //If the user chose to Divide the measurements, this method will run
+        public void DivideQuantity(int Divide)
+        {
+            D = Divide;
+            isDivide = true;
+            foreach (var ingredient in dictIngredient.ToList())
+            {
+                dictIngredient[ingredient.Key] /= Divide;
+            }
+
+            Console.WriteLine("Quantities have been Divided");
+            Console.WriteLine("\nUpdated quantities:");
+            foreach (var ingredient in dictIngredient)
+            {
+                Console.WriteLine($"{ingredient.Key}: {ingredient.Value} {dictType[ingredient.Key]}");
+            }
+        }
+
+        //If the User chose to Multiply the measurements, this method will run
+        public void MultiplyQuantity(int Multiply)
+        {
+            M = Multiply;
+            isMultiplied = true;
+            foreach (var ingredient in dictIngredient.ToList())
+            {
+                dictIngredient[ingredient.Key] *= Multiply;
+            }
+
+            Console.WriteLine("Quantities have been Multiplied");
+
+            //This outputs back to the user the new updated Quantities
+            Console.WriteLine("\nUpdated quantities:");
+            foreach (var ingredient in dictIngredient)
+            {
+                Console.WriteLine($"{ingredient.Key}: {ingredient.Value} {dictType[ingredient.Key]}");
+            }
+        }
+
+        public void ResetQuant()
+        {
+            //If the user previously had mutliplied the recipes ratio, this method will divide the recipe in reverse to counter it
+            if (isMultiplied==true)
+            {
+                foreach (var ingredient in dictIngredient.ToList())
+                {
+                    dictIngredient[ingredient.Key] /= M;
+                }
+
+                foreach (var ingredient in dictIngredient)
+                {
+                    Console.WriteLine($"{ingredient.Key}: {ingredient.Value} {dictType[ingredient.Key]}");
+                }
+
+            }
+            //If the user previously had divided the recipes ratio, this method will multiply the recipe in reverse to counter it
+            else if (isDivide==true) 
+            {
+                foreach (var ingredient in dictIngredient.ToList())
+                {
+                    dictIngredient[ingredient.Key] *= D;
+                }
+
+                foreach (var ingredient in dictIngredient)
+                {
+                    Console.WriteLine($"{ingredient.Key}: {ingredient.Value} {dictType[ingredient.Key]}");
+                }
+            }
+        }
+
+        //If the user chose to delete the recipe this method will run and clear the dictionaries
+        public void DeleteRecipe()
+        {
+            dictIngredient.Clear();
+            dictType.Clear();
+            Console.WriteLine("Recipes have been Wiped!");
+        }
+
+    }
+}
